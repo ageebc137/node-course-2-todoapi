d819f565c9a234702f9f1171a3d1f3f32b1288e1
Allister <ageebernal@aol.com>
2018-12-29 19:14:25 -0800

    add Delete /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://rickmortc137:Airaledn90!@ds145304.mlab.com:45304/nodetodoapi', {useNewUrlParser: true});
-
+mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}).then(() => {
+  console.log('Connected to Database')
+}).catch(err => {
+  console.log("not connected to database!", err);
+});
+// 'mongodb://localhost:27017/TodoApp'
+// 'mongodb://rickmortc137:Airaledn90!@ds145304.mlab.com:45304/nodetodoapi'
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -58,6 +58,24 @@ app.get('/todos/:id', (req, res) => {
   //error
     //400 - and send empty body back
 
+app.delete("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndDelete(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    console.log(todo);
+    res.send(todo);
+  }).catch((err) => {
+    res.status(400).send();
+  });
+
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
