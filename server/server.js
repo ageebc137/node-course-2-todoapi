000ba27199cd5bb8413fa3703635730de93c7e68
Allister <ageebernal@aol.com>
2018-12-29 23:18:37 -0800

    Add Patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {ObjectID} = require('mongodb');
@@ -73,6 +74,34 @@ app.delete("/todos/:id", (req, res) => {
   }).catch((err) => {
     res.status(400).send();
   });
+
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
 
 });
 
