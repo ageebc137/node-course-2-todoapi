78ba71a4c265bed98b9f2ec2fe6afdc7208ee7d5
Allister <ageebernal@aol.com>
2019-01-10 12:52:54 -0800

    Add Post /users/login route

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -6,7 +6,7 @@ var authenticate = (req, res, next) => {
   User.findByToken(token).then((user) => {
 
     if (!user) {
-      return Promise.reject(); d
+      return Promise.reject(); 
     }
     req.user = user;
     req.token = token;
